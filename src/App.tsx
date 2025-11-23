254fda9684fe36af78d59c43a64daf5c74e08311
Jhon Erwin Manis <manisjhonerwin@ymail.com>
2025-11-23 14:53:18 +0800

    remove unnecessary file

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import { PersonPage } from "./pages/PersonPage"
 
 export default function App() {
   return (
-    <BrowserRouter>
+   <BrowserRouter basename="/parents-dashboard">
       <Routes>
         <Route path="/" element={<DashboardPage />} />
         <Route path="/person/:name" element={<PersonPage />} />
