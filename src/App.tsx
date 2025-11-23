62a41ac9ffa95244a0ec26bf4e3257f69d25a0eb
Jhon Erwin Manis <manisjhonerwin@ymail.com>
2025-11-23 15:07:53 +0800

    Deploy

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import { PersonPage } from "./pages/PersonPage"
 
 export default function App() {
   return (
-   <BrowserRouter basename="/parents-dashboard">
+   <BrowserRouter basename="/family-contributions">
       <Routes>
         <Route path="/" element={<DashboardPage />} />
         <Route path="/person/:name" element={<PersonPage />} />
